292e4dbf327badf83c30b08782c08ad5cc7ab6d7
Javier Prieto Diaz <javier.diaz@ef.com>
2015-03-16 14:54:22 +0000

    fix for ie8 extending treshold to 1 sec

--- widget.js
+++ widget.js
@@ -94,7 +94,7 @@ define([
 							cued = false;
 						});
 				}
-				else if ( position !== duration && position > cue_out || duration - position <= 0.2) {
+				else if (position > cue_out || duration - position <= 1) {
 					cued = true;
 					return me
 						.emit("audio5js/do/pause")
